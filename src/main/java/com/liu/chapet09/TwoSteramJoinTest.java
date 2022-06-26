6382f25d057e9a1cc2149fad3fb1e95d37a86fd8
Cristiano <240939851@qq.com>
2022-06-26 21:23:46 +0800

    second-commit

--- src/main/java/com/liu/chapet09/TwoSteramJoinTest.java
+++ src/main/java/com/liu/chapet09/TwoSteramJoinTest.java
@@ -4,6 +4,8 @@ import com.liu.chapet05.Event;
 import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 
@@ -13,13 +15,16 @@ public class TwoSteramJoinTest {
     public static void main(String[] args) {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
-        env.fromElements(new Tuple3("a","stream_1",100l),
-                new Tuple3("b","stream_1",200l),
-                new Tuple3("c","stream_1",300l));
-
-
-
-
+        SingleOutputStreamOperator<Tuple3<String,String,Long>> source = env.fromElements(new Tuple3("a", "stream_1", 100l),
+                new Tuple3("b", "stream_1", 200l),
+                new Tuple3("c", "stream_1", 300l));
+        SingleOutputStreamOperator<Tuple3<String, String, Long>> tuple3SingleOutputStreamOperator = source.assignTimestampsAndWatermarks(WatermarkStrategy.<Tuple3<String, String, Long>>forBoundedOutOfOrderness(Duration.ZERO)
+                .withTimestampAssigner(new SerializableTimestampAssigner<Tuple3<String, String, Long>>() {
+                    @Override
+                    public long extractTimestamp(Tuple3<String, String, Long> stringStringLongTuple3, long l) {
+                        return stringStringLongTuple3.f2;
+                    }
+                }));
 
     }
 }
